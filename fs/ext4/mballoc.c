1027ebe6e3188ded02756f566df11f526e634856
Wang Jianchao <wangjianchao@kuaishou.com>
2026-02-07 00:32:03 +0800

    ext4: remove the 'group' parameter of ext4_trim_extent
    [ Upstream commit bd2eea8d0a6b6a9aca22f20bf74f73b71d8808af ]
    
    Get rid of the 'group' parameter of ext4_trim_extent as we can get
    it from the 'e4b'.
    
    Reviewed-by: Andreas Dilger <adilger@dilger.ca>
    Signed-off-by: Wang Jianchao <wangjianchao@kuaishou.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20210724074124.25731-2-jianchao.wan9@gmail.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Stable-dep-of: d63c00ea435a ("ext4: mark group as trimmed only if it was fully scanned")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -5570,8 +5570,7 @@ ext4_trim_group_seq_partial(struct super_block *sb, ext4_group_t group,
 				goto out;
 			}
 
-			ret = ext4_trim_extent(sb, start,
-					       next - start, group, &e4b);
+			ret = ext4_trim_extent(sb, start, next - start, &e4b);
 			if (ret && ret != -EOPNOTSUPP)
 				break;
 			ret = 0;

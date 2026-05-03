4eade457af807dc32b4624f71dd9a477c41a6d7c
ekkusa <dayesofficial@gmail.com>
2026-05-04 04:56:11 +0800

    drivers/gpu: Import mali-r38p1

--- drivers/misc/mediatek/gpu/Android.mk
+++ drivers/misc/mediatek/gpu/Android.mk
@@ -17,7 +17,7 @@ GPU_OPTS += CONFIG_MALI_MTK_GPU_BM_2=y
 GPU_OPTS += CONFIG_MALI_MTK_BRINGUP=y
 include $(LOCAL_PATH_B)/gpu_$(word 1,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali_$(word 2,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali-$(word 3,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/drivers/gpu/arm/midgard/Android.mk
 
-LOCAL_MTK_GPU_VERSION := mali_valhall_r32p1_mt6893
+LOCAL_MTK_GPU_VERSION := mali_valhall_r38p1_mt6893
 LOCAL_MTK_PLATFORM := $(word 4,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))
 GPU_OPTS := MTK_GPU_VERSION=$(LOCAL_MTK_GPU_VERSION) MTK_PLATFORM=$(LOCAL_MTK_PLATFORM)
 GPU_OPTS += CONFIG_MALI_REAL_HW=y
@@ -105,6 +105,16 @@ GPU_OPTS += CONFIG_MALI_MEMORY_GROUP_MANAGER=y
 include $(LOCAL_PATH_B)/gpu_$(word 1,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali_$(word 2,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali-$(word 3,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/drivers/base/arm/memory_group_manager/Android.mk
 
 LOCAL_MTK_GPU_VERSION := mali_valhall_r32p1
+GPU_OPTS := MTK_GPU_VERSION=$(LOCAL_MTK_GPU_VERSION)
+GPU_OPTS += CONFIG_MALI_PROTECTED_MEMORY_ALLOCATOR=y
+include $(LOCAL_PATH_B)/gpu_$(word 1,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali_$(word 2,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali-$(word 3,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/drivers/base/arm/protected_memory_allocator/Android.mk
+
+LOCAL_MTK_GPU_VERSION := mali_valhall_r38p1
+GPU_OPTS := MTK_GPU_VERSION=$(LOCAL_MTK_GPU_VERSION)
+GPU_OPTS += CONFIG_MALI_MEMORY_GROUP_MANAGER=y
+include $(LOCAL_PATH_B)/gpu_$(word 1,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali_$(word 2,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali-$(word 3,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/drivers/base/arm/memory_group_manager/Android.mk
+
+LOCAL_MTK_GPU_VERSION := mali_valhall_r38p1
 GPU_OPTS := MTK_GPU_VERSION=$(LOCAL_MTK_GPU_VERSION)
 GPU_OPTS += CONFIG_MALI_PROTECTED_MEMORY_ALLOCATOR=y
 include $(LOCAL_PATH_B)/gpu_$(word 1,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali_$(word 2,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/mali-$(word 3,$(subst _, ,$(LOCAL_MTK_GPU_VERSION)))/drivers/base/arm/protected_memory_allocator/Android.mk

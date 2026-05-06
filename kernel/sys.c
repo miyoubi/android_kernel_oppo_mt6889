b6be5070e7a44f89ae9a262a0b955a3e0210a2e4
ekkusa <dayesofficial@gmail.com>
2026-05-06 09:40:32 +0800

    kms

--- kernel/sys.c
+++ kernel/sys.c
@@ -1261,9 +1261,9 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4) ||
 	    !strncmp(current->comm, "uprobestats", 11)) {
-		strcpy(tmp.release, "5.10.239");
-		pr_debug("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
+		strcpy(tmp.release, "5.10.246");
+		pr_debug("fake uname: %s release=%s\n",
+			 current->comm, tmp.release);
 	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
